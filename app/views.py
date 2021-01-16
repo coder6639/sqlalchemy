1a1aca1eca5fb17f2d567994e2156ea6f393a4fa
igi <github@6639.33mail.com>
2021-01-16 18:11:28 +0100

    Update templates, add edit.html. Add routes and views

---
+++ app/views.py
@@ -0,0 +1,80 @@
+from flask import render_template, redirect, request, url_for
+from app.models import Book, Author, BookAuthors, add_data, edit_data
+from forms.forms import BookForm, AuthorForm
+
+
+def book_library_view():
+    books = Book.query.all()
+    return render_template("library.html", books=books)
+
+
+def add_author_view():
+    form = AuthorForm()
+    authors = Author.query.all()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            add_data("author")
+        return redirect(url_for("add_author"))
+    return render_template("addauthor.html", form=form, authors=authors)
+
+
+def add_book_view():
+    form = BookForm()
+    books = Book.query.all()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            add_data("book")
+        return redirect(url_for("add_book"))
+    return render_template("addbook.html", form=form, books=books)
+
+
+def add_relation_view():
+    authors = Author.query.all()
+    books = Book.query.all()
+    bookauthors = BookAuthors.query.all()
+    if request.method == "POST":
+        add_data("relation")
+        return redirect(url_for("add_relation"))
+    return render_template("bookauthors.html", authors=authors, books=books, bookauthors=bookauthors)
+
+
+def delete_book_view(number):
+    book = Book.query.get(number)
+    if request.method == "POST":
+        book.delete()
+        return redirect(url_for("add_book"))
+    return render_template("delete.html", item=book)
+
+
+def delete_author_view(number):
+    author = Author.query.get(number)
+    if request.method == "POST":
+        author.delete()
+        return redirect(url_for("add_author"))
+    return render_template("delete.html", item=author)
+
+
+def delete_relation_view(number):
+    relation = BookAuthors.query.get(number)
+    if request.method == "POST":
+        relation.delete()
+        return redirect(url_for("add_relation"))
+    return render_template("delete.html", item=relation)
+
+
+def edit_book_view(number):
+    book = Book.query.get(number)
+    form = BookForm()
+    if request.method == "POST":
+        edit_data(book, "book")
+        return redirect(url_for("add_book"))
+    return render_template("edit.html", book=book, form=form)
+
+
+def edit_author_view(number):
+    form = AuthorForm()
+    author = Author.query.get(number)
+    if request.method == "POST":
+        edit_data(author, "author")
+        return redirect(url_for("add_author"))
+    return render_template("edit.html", author=author, form=form)
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app.views import book_library_view, add_book_view, add_author_view, add_relation_view
-from app.views import delete_book_view, delete_author_view
+from app.views import delete_book_view, delete_author_view, delete_relation_view, edit_book_view, edit_author_view
 from app import app
 
 
@@ -31,6 +31,21 @@ def delete_book(number):
 @app.route("/delete/author/<int:number>/", methods=["GET", "POST"])
 def delete_author(number):
     return delete_author_view(number)
+
+
+@app.route("/delete/relation/<int:number>", methods=["GET", "POST"])
+def delete_relation(number):
+    return delete_relation_view(number)
+
+
+@app.route("/edit/book/<int:number>", methods=["GET", "POST"])
+def edit_book(number):
+    return edit_book_view(number)
+
+
+@app.route("/edit/author/<int:number>", methods=["GET", "POST"])
+def edit_author(number):
+    return edit_author_view(number)
 
 
 if __name__ == "__main__":
